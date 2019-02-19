f31c7f575b5bc6c401ab33f4eac93ec8ec0e9967
Will Sokolowski <will.sokolowski@gmail.com>
2019-02-19 13:54:27 -0700

    helmet config

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,27 @@
 import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const app = express();
 
-app.get('*', (req, res) => res.send('Hello World!'));
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+app.get('/', function (req, res, next) {
+    console.log('first function');
+    next();
+  }, function (req, res) {
+    console.log('second function');
+    res.send('Hello World!');
+  });
 app.listen(8000, () => console.log("Listening on port 8000!"));
