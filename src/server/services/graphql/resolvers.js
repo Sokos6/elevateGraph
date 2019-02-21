42e1f84d088db62a60e27980731d92c87fe023bf
Will Sokolowski <will.sokolowski@gmail.com>
2019-02-21 12:41:52 -0700

    mutation and resolver update

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,9 +1,20 @@
 const resolvers = {
     RootQuery: {
         posts(root, args, context) {
-            return [];
-        },
+            return posts; 
+        }, 
     },
+    RootMutation: {
+        addPost(root, { post, user }, context) {
+          const postObject = { 
+            ...post,
+            user,
+            id: posts.length + 1,
+          };
+          posts.push(postObject);
+          return postObject;
+        },
+    }
 };
 
 export default resolvers;
